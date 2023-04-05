ae98c3bc9263f0061e66d72abad1534f89e3c341
Md Shaquib Alam <shaquib475@gmail.com>
2023-04-05 21:00:15 +0530

    color

--- app/src/main/java/com/example/diagnosticapp/MainActivity.java
+++ app/src/main/java/com/example/diagnosticapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.Menu;
@@ -22,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Actionbar background color
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.teal_700)));
 
         // declear 12 button to go another activiy by click on imageView usnig internt and onclick()
 
