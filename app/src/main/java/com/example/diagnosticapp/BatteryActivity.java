a5afc9a2349963929ac332e434e2e03eb9b30ac6
Md Shaquib Alam <shaquib475@gmail.com>
2023-04-25 23:56:07 +0530

    battery information

--- app/src/main/java/com/example/diagnosticapp/BatteryActivity.java
+++ app/src/main/java/com/example/diagnosticapp/BatteryActivity.java
@@ -6,7 +6,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.TextView;
 
 
@@ -50,9 +52,94 @@ public class BatteryActivity extends AppCompatActivity {
 
                     float voltTemp= (float) (intent.getIntExtra("voltage",0)*0.001);
                     voltage.setText(voltTemp+ "V");
+
+                    setHealth(intent);
+
+
+                    batteryType.setText(intent.getStringExtra("technology"));
+
+                    getChargingSource(intent);
+
+                    float tempTemp = (float) intent.getIntExtra("temperature",-1)/10;
+                    temperature.setText(tempTemp + "°C");
+
+                    setChargingStatus(intent);
                 }
             }
         };
+    }
+
+    private void setChargingStatus(Intent intent) {
+
+        int statusTemp = intent.getIntExtra("status",-1);
+
+        switch (statusTemp)
+        {
+            case BatteryManager.BATTERY_HEALTH_UNKNOWN:
+                chargingStatus.setText("Unknown");
+                break;
+            case BatteryManager.BATTERY_STATUS_CHARGING:
+                chargingStatus.setText("Charging");
+                break;
+            case BatteryManager.BATTERY_STATUS_NOT_CHARGING:
+                chargingStatus.setText("Hot Charging");
+                break;
+            case BatteryManager.BATTERY_STATUS_FULL:
+                chargingStatus.setText("Full");
+                break;
+            default:
+                chargingStatus.setText("Null" );
+        }
+    }
+
+    private void getChargingSource(Intent intent) {
+
+        int sourceTemp = intent.getIntExtra("plugged",-1);
+        switch (sourceTemp){
+
+            case BatteryManager.BATTERY_PLUGGED_AC:
+                chargingSource.setText("AC");
+                break;
+            case BatteryManager.BATTERY_PLUGGED_USB:
+                chargingSource.setText("USB");
+                break;
+            case BatteryManager.BATTERY_PLUGGED_WIRELESS:
+                chargingSource.setText("Wireless");
+                break;
+            default:
+                chargingSource.setText("Null");
+
+
+        }
+    }
+
+    private void setHealth(Intent intent) {
+        int val = intent.getIntExtra("health",0);
+
+        switch (val)
+        {
+            case BatteryManager.BATTERY_HEALTH_UNKNOWN:
+                health.setText("Unknown");
+                break;
+            case BatteryManager.BATTERY_HEALTH_GOOD:
+                health.setText("Good");
+                break;
+            case BatteryManager.BATTERY_HEALTH_OVERHEAT:
+                health.setText("Overheat");
+                break;
+            case BatteryManager.BATTERY_HEALTH_DEAD:
+                health.setText("Dead");
+                break;
+            case BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE:
+                health.setText("Over Voltage");
+                break;
+            case BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE:
+                health.setText("Unspecified Failure");
+                break;
+            case BatteryManager.BATTERY_HEALTH_COLD:
+                health.setText("Cold");
+                break;
+        }
     }
 
     @Override
