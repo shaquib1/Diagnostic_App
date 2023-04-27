a6e3d8263d854c8ac391734b822bddaa668471a3
Md Shaquib Alam <shaquib475@gmail.com>
2023-04-27 18:04:29 +0530

    check volumes

--- app/src/main/java/com/example/diagnosticapp/Mobile_infoActivity.java
+++ app/src/main/java/com/example/diagnosticapp/Mobile_infoActivity.java
@@ -1,14 +1,51 @@
 package com.example.diagnosticapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
 
 public class Mobile_infoActivity extends AppCompatActivity {
 
+
+    private TextView phoneNumberTextView;
+    private TextView imeiTextView;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mobile_info);
+
+
+        phoneNumberTextView = findViewById(R.id.phone_number_value_textview);
+        imeiTextView = findViewById(R.id.imei_textview);
+
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        String phoneNumber = telephonyManager.getLine1Number();
+        String imeiNumber = telephonyManager.getDeviceId();
+
+        phoneNumberTextView.setText(phoneNumber);
+        imeiTextView.setText(imeiNumber);
     }
 }
+
+
--- app/src/main/java/com/example/diagnosticapp/SoundActivity.java
+++ app/src/main/java/com/example/diagnosticapp/SoundActivity.java
@@ -1,16 +1,114 @@
 package com.example.diagnosticapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class SoundActivity extends AppCompatActivity {
 
+    private SeekBar ringVolumeSeekBar,
+            musicVolumeSeekBar,
+            alarmVolumeSeekBar,
+            notificationVolumeSeekBar;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound);
 
         getSupportActionBar().setTitle("Sound");
+
+        ringVolumeSeekBar = findViewById(R.id.ring_volume_seekbar);
+        musicVolumeSeekBar = findViewById(R.id.music_volume_seekbar);
+        alarmVolumeSeekBar = findViewById(R.id.alarm_volume_seekbar);
+        notificationVolumeSeekBar = findViewById(R.id.notification_volume_seekbar);
+
+        AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        int maxRingVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_RING);
+        int currentRingVolume = audioManager.getStreamVolume(AudioManager.STREAM_RING);
+        ringVolumeSeekBar.setMax(maxRingVolume);
+        ringVolumeSeekBar.setProgress(currentRingVolume);
+
+        int maxMusicVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentMusicVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        musicVolumeSeekBar.setMax(maxMusicVolume);
+        musicVolumeSeekBar.setProgress(currentMusicVolume);
+
+        int maxAlarmVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_ALARM);
+        int currentAlarmVolume = audioManager.getStreamVolume(AudioManager.STREAM_ALARM);
+        alarmVolumeSeekBar.setMax(maxAlarmVolume);
+        alarmVolumeSeekBar.setProgress(currentAlarmVolume);
+
+        int maxNotificationVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_NOTIFICATION);
+        int currentNotificationVolume = audioManager.getStreamVolume(AudioManager.STREAM_NOTIFICATION);
+        notificationVolumeSeekBar.setMax(maxNotificationVolume);
+        notificationVolumeSeekBar.setProgress(currentNotificationVolume);
+
+        ringVolumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_RING, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        musicVolumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        alarmVolumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_ALARM, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        notificationVolumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_NOTIFICATION, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
     }
 }
